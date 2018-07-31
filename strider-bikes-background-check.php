6637d0857fcda3242f6cc6a26c48ff5e92d07e8f
Nick Williams <nick.williams@striderhq.local>
2018-07-31 13:03:29 -0600

    debuggin

--- strider-bikes-background-check.php
+++ strider-bikes-background-check.php
@@ -219,8 +219,8 @@ class Strider_Bikes_Background_Check{
                 $lp_course = LP_Course::get_course($c);
                 $user_grade = $lp_course->evaluate_course_results($i->ID);
                 //echo $user_grade . ' ' . $lp_course->passing_condition . ' ';
-                if($bgStatus == 1){
-                    $out2 .= '<p>'.$i->display_name.'</p><p>'.$i->user_email.'</p>' . '<p>'.get_the_title($c).': </p>' . '<p>' . $user_grade . '</p>';
+                if($user_grade == 100 && $bgStatus == 1){
+                    $out2 .= '<p>'.$i->display_name.'</p><p>'.$i->user_email.'</p>' . '<p>'.get_the_title($c).': ' . $user_grade . '</p>';
                     $out2 .= '<div> <a href="https://www.striderbikes.com/_education/wp-admin/user-edit.php?user_id='.$i->ID.'"><p>edit user</p></a> </div><br/>'; 
                 }
         }
