f70643f4a7219a048fe03f049bac509ab2f9b6ec
Nick Williams <nick.williams@striderhq.local>
2018-08-30 14:58:38 -0600

    lala

--- strider-bikes-background-check.php
+++ strider-bikes-background-check.php
@@ -70,7 +70,7 @@ class Strider_Bikes_Background_Check{
         add_action('wp_ajax_check_order_status', array($this, 'sb_bg_check_order_status'));
         add_action('wp', array($this, 'restrict_until_complete_maybe'));
         add_action('wp', array($this, 'add_menu_filter'));
-        add_filter('learn_press_user_passed_course_condition', array($this, 'check_if_course_passed'),10, 3);
+        add_action('striderbikes_learnpress_passed_course', array($this, 'check_if_course_passed'),20, 1);
     }
     //hooks into on complete process of woo
 
